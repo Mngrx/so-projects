407f6a0d6a3b877fc224d46f15be06aad973045d
Igor Barbosa Nogueira <igornogueir@gmail.com>
2018-03-06 22:48:18 -0300

    Falta a parte do Json e parentes do processo

--- projeto1/parte2/main.cpp
+++ projeto1/parte2/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdlib>
-#include <fstream>  
+#include <sstream>  
 #include <unistd.h>
 #include <sys/types.h>
 #include <vector>
@@ -15,42 +15,86 @@ typedef struct {
 
 } UserAndProcess;
 
-
-
-int numberSystemProcess() {
-    return system("ps aux | wc -l");
+// Função que executa um comando no terminal e retorna o que seria printado como string
+// Creditos: https://stackoverflow.com/questions/32039852/returning-output-from-bash-script-to-calling-c-function
+std::string exec(char *cmd)
+{
+    FILE *pipe = popen(cmd, "r");
+    if (!pipe)
+        return "ERROR";
+    char buffer[128];
+    std::string result = "";
+    while (!feof(pipe))
+    {
+        if (fgets(buffer, 128, pipe) != NULL)
+            result += buffer;
+    }
+    pclose(pipe);
+    return result;
 }
 
-void numberSystemProcessByUser() {
-    
-    system("ps -e -o user > data.txt");
-    
-    ifstream reader("data.txt", std::ifstream::in);
+string numberSystemProcess() {
+    char * command = "ps aux | wc -l";
+    return exec(command);
+}
 
-    string aux;
+void printNumberSystemProcess() {
+    cout << "Total de processos: " << numberSystemProcess();
+}
+
+vector<UserAndProcess> numberSystemProcessByUser() {
+
+    char * command = "ps -e -o user | sort | uniq -c";
+
+    string retornoDosUsuarios = exec(command);
+
+    std::istringstream reader(retornoDosUsuarios);
 
     vector<UserAndProcess> vectorOfCounting;
 
-    while (reader >> aux) {
+    int numberOfProcess;
+    string user;
+
+    UserAndProcess aux ;
+
+
+    while (reader >> numberOfProcess) {
         
-        if (aux == "USER")
+        reader >> user;
+
+        if (user == "USER" && numberOfProcess == 1) 
             continue;
 
-        cout <<  aux << endl;
+        aux.numberOfProcess = numberOfProcess;
+        aux.userName = user;
 
+        vectorOfCounting.push_back(aux);
     }
 
+    return vectorOfCounting;
 
+}
 
-    system("rm data.txt");
+void printNumberSystemProcessByUser() {
+
+    vector<UserAndProcess> vectorOfCounting = numberSystemProcessByUser();
+
+    cout << "Processos por usuário:\n";
+
+    for (auto i : vectorOfCounting) {
+        cout << i.userName << ": " << i.numberOfProcess << endl;
+    }
 
 }
 
 
 int main (void) {
 
-    //std::cout << numberSystemProcess() << std::endl;
-    numberSystemProcessByUser() ;
+    printNumberSystemProcessByUser();
+
+    cout << "\n=======================================\n";
+
+    printNumberSystemProcess() ;
 
     return 0;
 
